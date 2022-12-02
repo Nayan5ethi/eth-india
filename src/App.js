7832b1c5f38b6579e77d384908c8a3b7b39c75e3
drunckj <dwivediswastik@gmail.com>
2022-12-03 01:05:46 +0530

    new dashboard

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/dashboard/Dashboard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PropertyListing from './components/PropertyListing';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
-  <Dashboard/>
+    <ChakraProvider>
+    <BrowserRouter>
+    <Routes>
+<Route path='/' element={<Dashboard/>}>
+
+  <Route path='listing' element={<PropertyListing/>} ></Route>
+</Route>
+
+
+
+    </Routes>
+  </BrowserRouter>
+  </ChakraProvider>
   );
 }
 
